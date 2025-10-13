acb5778563866cf1b0072d8e688e0c8469ad1b04
ayuxi03 <ayushimoitra03@gmail.com>
2025-10-13 14:48:34 +0530

    Add user selection and total messages display to app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import preprocessor
+import preprocessor, helper
 
 st.sidebar.title("WhatsApp Chat Analyzer")
 
@@ -13,6 +13,24 @@ if uploaded_file is not None:
 
     # Preprocess data using preprocessor module
     df = preprocessor.preprocess(data)
-    
+
     # Display the dataframe on the app
     st.dataframe(df)
+
+    # Fetch unique users
+    user_list = df['user'].unique().tolist()
+    user_list.remove('whatsapp_notification')
+    user_list.sort()
+    user_list.insert(0, "Overall")
+
+    selected_user = st.sidebar.selectbox("Show analysis wrt", user_list)
+
+    if st.sidebar.button("Show Analysis"):
+
+        num_messages = helper.fetch_stats(selected_user, df)
+        
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1:
+            st.header("Total Messages")
+            st.title(num_messages)
