674f6c070e7d42da2d5698315fda18e00610078f
ayuxi03 <ayushimoitra03@gmail.com>
2025-10-14 01:39:49 +0530

    Restructured helper function for cleaner and shorter code

--- helper.py
+++ helper.py
@@ -1,25 +1,16 @@
 def fetch_stats(selected_user, df):
 
-    if selected_user == 'Overall':
-        # 1. Fetching number of messages
-        num_messages = df.shape[0]
-
-        # 2. number of words
-        words = []
-        for message in df['message']:
-            # Splitting each message into words and adding to the list
-            words.extend(message.split())
-        
-        return num_messages, len(words)
+    # if specific user is selected, dataframe is filtered for that user
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
     
-    else:
-        # Fetching number of messages for the selected user
-        new_df = df[df['user'] == selected_user]
-        num_messages = new_df.shape[0]
+    # 1. Fetching number of messages
+    num_messages = df.shape[0]
 
-        # number of words for the selected user
-        words = []
-        for message in new_df['message']:
-            words.extend(message.split())
-            
-        return num_messages, len(words)
+    # 2. number of words
+    words = []
+    for message in df['message']:
+        # Splitting each message into words and adding to the list
+        words.extend(message.split())
+
+    return num_messages, len(words)
