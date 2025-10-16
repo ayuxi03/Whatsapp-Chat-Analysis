ce84d89bd96a48cfd1872bc72183fe22ace1c345
ayuxi03 <ayushimoitra03@gmail.com>
2025-10-17 02:22:02 +0530

    Add emoji analysis, activity timeline, filtered wordcloud to remove emojis, special chars

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import preprocessor, helper
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
 
 st.sidebar.title("WhatsApp Chat Analyzer")
+st.set_page_config(layout="wide")
 
 uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
@@ -14,9 +16,6 @@ if uploaded_file is not None:
 
     # Preprocess data using preprocessor module
     df = preprocessor.preprocess(data)
-
-    # Display the dataframe on the app
-    st.dataframe(df)
 
     # Fetch unique users
     user_list = df['user'].unique().tolist()
@@ -31,6 +30,9 @@ if uploaded_file is not None:
 
     # When the button is clicked, perform analysis
     if st.sidebar.button("Show Analysis"):
+
+        # Stats Area
+        st.title("Top Statistics")
 
         # Fetch stats using helper module
         num_messages, num_words, num_media_messages, num_links = helper.fetch_stats(selected_user, df)
@@ -53,6 +55,15 @@ if uploaded_file is not None:
         with col4:
             st.header("Links Shared")
             st.title(num_links)
+        
+
+        # Montly activity timeline
+        st.title('Monthly Activity Timeline')
+        timeline = helper.monthly_timeline(selected_user, df)
+        fig, ax = plt.subplots(figsize=(15,5))
+        ax.plot(timeline['time'], timeline['message_count'])
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
 
         # Finding the most active user in the group (only for Overall)
         if selected_user == 'Overall':
@@ -65,7 +76,7 @@ if uploaded_file is not None:
             col1, col2 = st.columns(2)
 
             with col1:
-                ax.bar(x.index, x.values) # Bar chart using the axis
+                ax.bar(x.index, x.values, color=(0.5,0.3,0.5)) # Bar chart using the axis
                 plt.xticks(rotation='vertical') # Rotate x-axis labels vertically
                 st.pyplot(fig)
             
@@ -77,12 +88,35 @@ if uploaded_file is not None:
         st.title("Wordcloud")
         df_wc = helper.create_wordcloud(selected_user, df)
         fig, ax = plt.subplots()
-        ax.imshow(df_wc)
+        ax.imshow(df_wc) # Display the word cloud image
         st.pyplot(fig)
 
 
-        # most common words
+        # Displaying dataframe of most common words
         st.title("Most Common Words")
         most_common_df = helper.most_common_words(selected_user, df)
+        fig, ax = plt.subplots(figsize=(10,5))
+        ax.barh(most_common_df[0], most_common_df[1], color=(0.5,0.8,0.5)) # Bar chart of most common words
+        st.pyplot(fig)
 
-        st.dataframe(most_common_df)
+
+        # emoji analysis
+        st.title("Emoji Analysis")
+        emoji_df = helper.emoji_helper(selected_user, df)
+
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.dataframe(emoji_df) # Display dataframe of emojis and their counts
+
+        with col2:
+            fig, ax = plt.subplots()
+            plt.rcParams['font.family'] = 'Segoe UI Emoji'
+            ax.pie(
+                emoji_df['count'].head(),
+                labels=emoji_df['emoji'].head(),
+                autopct="%0.2f",
+                textprops={'fontsize': 15},
+                colors=["#ab2a2a", "#fb9145", "#defb6b", "#66c29a", '#5e4fa2']) # Pie chart of top emojis
+            st.pyplot(fig)
+
--- helper.py
+++ helper.py
@@ -2,6 +2,8 @@ from urlextract import URLExtract
 from wordcloud import WordCloud
 import pandas as pd
 from collections import Counter
+import emoji
+import re
 
 extractor = URLExtract()
 
@@ -43,15 +45,27 @@ def fetch_most_active_users(df):
 
 def create_wordcloud(selected_user, df):
 
+    # 6. Creating a word cloud of the most commonly used words
+
     if selected_user != 'Overall':
         df = df[df['user'] == selected_user]
     
+    # removing group notifications and media omitted messages
     temp = df[df['user'] != 'whatsapp_notification']
     temp = temp[~temp['message'].isin(['<Media omitted>\n', 'You deleted this message\n', 'This message was deleted\n'])]
 
-    f = open('banglish_hinglish_stopwords.txt', 'r')
-    stop_words = f.read().splitlines()
+    # using custom stop words list from banglish_hinglish_stopwords.txt
+    with open('banglish_hinglish_stopwords.txt', 'r') as f:
+        stop_words = f.read().splitlines()
 
+    # Combine all messages from temp['message'] into a string separated by " "
+    text = temp['message'].str.cat(sep=" ") # same as " ".join(temp['message'])
+
+    # Keep only alphabetic words (removes emojis, numbers, punctuation)
+    text = " ".join(re.findall(r'\b[a-zA-Z]+\b', text))
+
+
+    # Generate the word cloud
     wc = WordCloud(
         width=400,
         height=200,
@@ -61,31 +75,67 @@ def create_wordcloud(selected_user, df):
         colormap='Spectral_r',
         stopwords=set(stop_words))
     
-    df_wc = wc.generate(temp['message'].str.cat(sep=" "))
+    df_wc = wc.generate(text)
 
     return df_wc
 
+
 def most_common_words(selected_user, df):
+
+    # 7. Finding the top 20 most common words
 
     if selected_user != 'Overall':
         df = df[df['user'] == selected_user]
     
+    # removing group notifications and media omitted messages
     temp = df[df['user'] != 'whatsapp_notification']
     temp = temp[~temp['message'].isin(['<Media omitted>\n', 'You deleted this message\n', 'This message was deleted\n'])]
 
-    # remove stop words
-    # stopwords filter
-    
+    # using custom stop words list from banglish_hinglish_stopwords.txt
     f = open('banglish_hinglish_stopwords.txt', 'r')
     stop_words = f.read()
 
+    # Collecting all words in a list after removing stop words
     words = []
 
     for message in temp['message']:
         for word in message.lower().split():
-            if word not in stop_words:
+            if word not in stop_words and not emoji.is_emoji(word):
                 words.append(word)
 
+    # Using Counter to get the most common words
     most_common_df = pd.DataFrame(Counter(words).most_common(20))
 
     return most_common_df
+
+
+def emoji_helper(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+    
+    emojis = []
+    for message in df['message']:
+        emojis.extend([c for c in message if emoji.is_emoji(c)])
+
+    # Count each emoji occurrences
+    emoji_df = pd.DataFrame(Counter(emojis).most_common(20)).rename(columns={0: 'emoji', 1: 'count'})
+
+    return emoji_df
+
+
+def monthly_timeline(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    # Add message count column by month
+    timeline = df.groupby(['year', 'month_num', 'month']).count()['message'].reset_index()
+    timeline.rename(columns = {'message': 'message_count'}, inplace=True)
+
+    # create month-year column
+    time=[]
+    for i in range(timeline.shape[0]):
+        time.append(timeline['month'][i] + "-" + str(timeline['year'][i]))
+
+    timeline['time'] = time
+
+    return timeline
